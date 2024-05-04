21a719895c9104d5b52b4a12c1b1b747efaff049
vankiet318 <vankietnguyen318@gmail.com>
2024-05-04 20:51:46 +0700

    create 3d grid and right UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Canvas } from '@react-three/fiber'
-import { KeyboardControls, OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import Husk from './loaders/Husk/Husk'
-import LevelStage from './components/LevelStage/LevelStage';
+import { KeyboardControls, OrbitControls, PerspectiveCamera, Html } from '@react-three/drei';
 import { Suspense, useMemo } from 'react';
-import CharacterController from './components/CharacterController/CharacterController';
 import { Physics } from '@react-three/rapier';
 
 
@@ -15,7 +12,9 @@ function App() {
     { name: "left", keys: ["KeyA"] },
     { name: "right", keys: ["KeyD"] },
   ], []);
-
+  const size = 6;
+  const divisions = 6;
+  const color = "gray"
   return (
     <div id="canvas-container">
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [5, 15, 10] }}>
@@ -29,7 +28,40 @@ function App() {
           <Physics debug>
             
           {/* TODO: create grid box 3d & right UI */}
-            
+          <group position={[-10, -5, 0]}>
+              <gridHelper
+                  args={[size, divisions, color]}
+                  position={[size / 2, 0, size / 2]} />
+              <gridHelper
+                  args={[size, divisions, color]}
+                  rotation={[Math.PI / 2, 0, Math.PI / 2]}
+                  position={[0, 3 * size / 2, size / 2]} />
+              <gridHelper
+                  args={[size, divisions, color]}
+                  rotation={[Math.PI / 2, 0, Math.PI / 2]}
+                  position={[0, size / 2, size / 2]} />
+              <gridHelper
+                  args={[size, divisions, color]}
+                  rotation={[Math.PI / 2, 0, 0]}
+                  position={[size / 2, 3 * size / 2, 0]} />
+              <gridHelper
+                  args={[size, divisions, color]}
+                  rotation={[Math.PI / 2, 0, 0]}
+                  position={[size / 2, size / 2, 0]} />
+            </group>
+              <Html position={[5, 10, 0]} className='instructions-label'>
+                <ul>
+                    <li><strong>Drag:</strong> <span>Mouse</span></li>
+                    <li><strong>Rotate:</strong>
+                        <ul>
+                            <li><strong>X-axis:</strong> Q</li>
+                            <li><strong>Y-axis:</strong> E</li>
+                            <li><strong>Z-axis:</strong> R</li>
+                        </ul>
+                    </li>
+                    <li><strong>Hard Drop:</strong> <span>Space</span></li>
+                </ul>
+              </Html>
           </Physics>
         </Suspense>
       </Canvas>
