88a6f25c1bad7147f9b042d7389e93506683356f
vankiet318 <vankietnguyen318@gmail.com>
2024-05-09 10:27:34 +0700

    add tetrimino

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Canvas } from '@react-three/fiber'
 import { KeyboardControls, OrbitControls, PerspectiveCamera, Html } from '@react-three/drei';
 import { Suspense, useMemo } from 'react';
 import { Physics } from '@react-three/rapier';
-
+import { Box } from '@react-three/drei';
+import * as THREE from 'three';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
   const size = 6;
   const divisions = 6;
   const color = "gray"
+  const blockColor = new THREE.Color("purple");
   return (
     <div id="canvas-container">
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [5, 15, 10] }}>
@@ -49,19 +51,25 @@ function App() {
                   rotation={[Math.PI / 2, 0, 0]}
                   position={[size / 2, size / 2, 0]} />
             </group>
-              <Html position={[5, 10, 0]} className='instructions-label'>
-                <ul>
-                    <li><strong>Drag:</strong> <span>Mouse</span></li>
-                    <li><strong>Rotate:</strong>
-                        <ul>
-                            <li><strong>X-axis:</strong> Q</li>
-                            <li><strong>Y-axis:</strong> E</li>
-                            <li><strong>Z-axis:</strong> R</li>
-                        </ul>
-                    </li>
-                    <li><strong>Hard Drop:</strong> <span>Space</span></li>
-                </ul>
-              </Html>
+            <Html position={[5, 10, 0]} className='instructions-label'>
+              <ul>
+                  <li><strong>Drag:</strong> <span>Mouse</span></li>
+                  <li><strong>Rotate:</strong>
+                      <ul>
+                          <li><strong>X-axis:</strong> Q</li>
+                          <li><strong>Y-axis:</strong> E</li>
+                          <li><strong>Z-axis:</strong> R</li>
+                      </ul>
+                  </li>
+                  <li><strong>Hard Drop:</strong> <span>Space</span></li>
+              </ul>
+            </Html>
+            <group>
+              <Box position={[-1, 0, 0]} color={blockColor}/>
+              <Box position={[0, 0, 0]} color={blockColor} />
+              <Box position={[1, 0, 0]} color={blockColor} />
+              <Box position={[1, 1, 0]} color={blockColor} />
+            </group>
           </Physics>
         </Suspense>
       </Canvas>
